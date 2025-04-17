f8e486b5910f749b8ea7f86bdbe191476cb7fccf
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-04-17 14:28:06 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,7 +12,12 @@ public class Program {
 
 		
 		Departament obj = new Departament(1, "Books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Bob", "hique@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(seller);
+		
+		
 		
 	}
 
