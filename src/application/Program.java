4d241968d985940df0a7e85bbe45c7b0f76cdce7
Henrique Milioli Ronchi <hiquemilioli@gmail.com>
2025-04-18 13:08:15 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,11 +13,11 @@ public class Program {
 		// TODO Auto-generated method stub
 
 		
-		Department obj = new Department(1, "Books");
 		
-		Seller seller = new Seller(21, "Bob", "hique@gmail.com", new Date(), 3000.0, obj);
 		
 		SellerDao sellerDao =  DaoFactory.creatSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 		
